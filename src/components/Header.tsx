c6fe4dcb97a9da18d3a786b0698ada606078ae48
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2023-06-27 12:29:02 +0200

    header scss variables utility classes

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import '../assets/styles/Header.scss';
+
+type Props = {
+  script: string,
+}
+
+export default function Header({ script }: Props) {
+  return (
+    <header className="header">
+      <div className="logo">
+        <h2>
+          APMM <br />
+          Process
+        </h2>
+      </div>
+      <div className="title">
+        <h2 className="title-text">{ script }</h2>
+      </div>
+    </header>
+  )
+}
